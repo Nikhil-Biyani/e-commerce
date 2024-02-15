7ee6a6cbd2cfb0c01c9289a1a7af72c4a1722a11
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-02-16 00:00:56 +0530

    Added authorization and login feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
+import JWT from 'jsonwebtoken';
+
+// POST REGISTER
 
 export const registerController = async(req, res) => {
     try {
@@ -60,3 +63,59 @@ export const registerController = async(req, res) => {
         })
     }
 };
+
+// POST LOGIN
+
+export const loginController = async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        // validation
+        if(!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid email or password'
+            });
+        }
+        // Check user
+        const user = await userModel.findOne({email});
+        if(!user) {
+            return res.status(404).send({
+                success: false,
+                message: 'Email is not Registered'
+            });
+        }
+        
+        const match = await comparePassword(password, user.password);
+        if(!match) {
+            return res.status(200).send({
+                success: false,
+                message: 'Invalid password'
+            });
+        }
+
+        // Create Token
+        const token = await JWT.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d"});
+        res.status(200).send({
+            success: true,
+            message: 'Successfully Logged in',
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address
+            },
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message: 'Error in Login',
+            error
+        });
+    }
+};
+
+export const testController = (req, res) => {
+    res.send("Protected Route");
+}
